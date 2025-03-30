9935dcf45497ff46b49020e9538432bdd75798b3
Ryan Perry <rperry21@vols.utk.edu>
2025-03-30 01:33:38 -0400

    code should work for swap.asm, avg.asm, calc.asm

--- vmas26/Assembler.cs
+++ vmas26/Assembler.cs
@@ -171,12 +171,33 @@ namespace Instruction {
 
     // CGW: Represents a Swap instruction with two optional parameters.
     public class Swap : IInstruction {
+        /*
         private Nullable<int> first, second;
         public Swap(Nullable<int> first, Nullable<int> second) {
             this.first = first;
             this.second = second;
         }
-        public int Encode() => unchecked((int)(0x01000000 | ((first ?? 4) & 0xFFF) << 12 | ((second ?? 0) & 0xFFF)));
+        public int Encode() {
+            int fromVal = (first ?? 4);
+            int toVal = (second ?? 0);
+
+            int encodedFrom = (fromVal >> 2) & 0xFFF;
+            int encodedTo = (toVal >> 2) & 0xFF;
+
+            return unchecked(0x01000000 | (encodedFrom << 12) | encodedTo);
+        }
+        */
+        private int from, to;
+
+        public Swap(Nullable<int> first, Nullable<int> second) {
+            this.from = (first ?? 4) >> 2;
+            this.to = (second ?? 0) >> 2;
+        }
+        public int Encode() {
+            int encodedFrom = from & 0xFFF;
+            int encodedTo = to & 0xFFF;
+            return (0x01000000 | (encodedFrom << 12) | encodedTo);
+        }
     }
 
     // CGW: Represents a simple Input instruction.
